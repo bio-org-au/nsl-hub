89aae7a1132ad07b7dd33f0f510ae041603fb77f
Greg Clarke <gregbclarke@gmail.com>
2016-10-15 13:28:28 +1100

    Config changes for deployment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,3 +40,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+
+# Rails.application.config.plants_url = "http://localhost:3000/" 
+# Rails.application.config.mosses_url = "http://localhost:3001/" 
