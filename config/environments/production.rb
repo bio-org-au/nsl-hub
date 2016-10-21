022f15ac140ad6b774fc210bddf411f044422891
Greg Clarke <gregbclarke@gmail.com>
2016-10-21 11:45:22 +1100

    Add relative_url_root to production config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_controller.relative_url_root = "/nsl/hub"
 end
 
 load "#{ENV['HOME']}/.nsl/hub-config.rb"
